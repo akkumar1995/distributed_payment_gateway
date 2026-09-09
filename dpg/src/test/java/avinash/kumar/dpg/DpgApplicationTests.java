10d9df7cd9d3a1d0819d30bed6efbde92818ff54
akkumar1995 <avinashhwpm@gmail.com>
2026-09-09 18:01:27 +0530

    initial commit

---
+++ dpg/src/test/java/avinash/kumar/dpg/DpgApplicationTests.java
@@ -0,0 +1,13 @@
+package avinash.kumar.dpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
